9014bc3e4545aee90d44122fb98df68b3c0b1fce
Otilia-Torgby <otorgby@st.ug.edu.gh>
2024-05-18 13:46:11 +0100

    Added the userInfo file

---
+++ userInfo.js
@@ -0,0 +1,20 @@
+// userInfo.js
+
+function createUserProfiles(names, modifiedNames) {
+    const userProfiles = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const originalName = names[i];
+      const modifiedName = modifiedNames[i];
+      const id = i + 1; // Auto-incremented ID starting from 1
+      userProfiles.push({ originalName, modifiedName, id });
+  }
+
+  return userProfiles;
+}
+
+// Usage:
+const namesArray = ["OTILIA", "TORGBY", "PATIENCE"];
+const modifiedNamesArray = ["'otilia", "TORGBY", "patience"];
+const profiles = createUserProfiles(namesArray, modifiedNamesArray);
+console.log(profiles);
