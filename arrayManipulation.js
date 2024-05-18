8b1bfba63e89e27615d9b2d4efc1973710d88eec
Otilia-Torgby <otorgby@st.ug.edu.gh>
2024-05-18 13:16:37 +0100

    Modified the arrayManipulation file

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,4 +1,5 @@
 // arrayManipulation.js
+// Task 1
 
 function processArray(arr) {
     // Create a new array to store the modified values
@@ -22,3 +23,32 @@ function processArray(arr) {
 const inputArray = [5,10,15,20,25,30];
 const modifiedArray = processArray(inputArray);
 console.log(modifiedArray);         
+
+
+// Task 2
+function formatArrayStrings(stringArray, numberArray) {
+    // Create a new array to store the modified strings
+    const result = [];
+  
+    // Iterate through the input arrays
+    for (let i = 0; i < stringArray.length; i++) {
+      const str = stringArray[i];
+      const num = numberArray[i];
+  
+      if (num % 2 === 0) {
+        // capitalize the entire string if the number is even
+      result.push(str.toUpperCase());
+    } else {
+      // convert the string to lowercase if the number is odd
+      result.push(str.toLowerCase());
+    }
+  }
+
+  return result;
+}
+
+// Usage:
+const strings = ["Otilia", "Torgby", "Patience"];
+const numbers = [3, 6, 9];
+const modifiedStrings = formatArrayStrings(strings, numbers);
+console.log(modifiedStrings); 
